aa80371fc7bdc6f13b00c4496b85d5119fb04df5
heiliglied <heiliglied@gmail.com>
2017-10-31 22:47:52 +0900

    서버 클라이언트 추가.

--- ChatBot-Chat/app.js
+++ ChatBot-Chat/app.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log('Hello world');
+require('dotenv').config({ path: 'configuration.env' });
+const path = require('path');
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+const port = process.env.PORT || 6974;
+
+var server = http.createServer(function (request, response) {
+    var querystring = url.parse(request.url, true);
+    var queries = querystring.query;
+    console.log(queries);
+}).listen(port);
